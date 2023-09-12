94054af1ab8dc1dc48555211765b6fbebfde6eb6
Facundo Lobo <faculobo200@gmail.com>
2023-09-11 22:15:08 -0300

    Added inventory events

--- Assets/Scripts/Game/GameEvent.cs
+++ Assets/Scripts/Game/GameEvent.cs
@@ -1,13 +1,27 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Objects/GameEvent")]
 public class GameEvent : ScriptableObject
 {
+    public enum ItemAction
+    {
+        Equip = 0,
+        Remove
+    };
+
     [SerializeField] private ItemData[] items;
 
     public ItemData[] Items => items;
 
     #region EVENTS
+    public event Action<Inventory> OnInventoryChanged;
+    public void InventoryChanged(Inventory inventory) { OnInventoryChanged?.Invoke(inventory); }
 
+    public event Action<Inventory> OnInventoryToggle;
+    public void InventoryToggle(Inventory inventory) { OnInventoryToggle?.Invoke(inventory); }
+
+    public event Action<Inventory, Item, ItemAction> OnItemSelected;
+    public void ItemSelected(Inventory inventory, Item item, ItemAction action) { OnItemSelected?.Invoke(inventory, item, action); }
     #endregion
 }
