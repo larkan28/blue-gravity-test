0b5480a555ef38754de08560f8b6fab54931b108
Facundo Lobo <faculobo200@gmail.com>
2023-09-11 22:16:04 -0300

    Added max items capacity and events

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private int maxCapacity;
+    [SerializeField] private GameEvent gameEvent;
+
+    public int Count => Items.Count;
+    public int Capacity => maxCapacity;
+
     public readonly List<Item> Items = new();
 
     public void Add(ItemData data, int quantity = 1)
@@ -10,17 +16,29 @@ public class Inventory : MonoBehaviour
         if (data == null)
             return;
 
+        if (quantity < 1)
+            quantity = 1;
+
         Item existingItem = Items.Find(x => x.Data == data);
 
         if (existingItem != null)
+        {
             existingItem.Quantity += quantity;
-        else
+            gameEvent.InventoryChanged(this);
+        }
+        else if (Count < maxCapacity)
+        {
             Items.Add(new Item(data, quantity));
+            gameEvent.InventoryChanged(this);
+        }
     }
 
     public void Remove(Item item)
     {
         if (item != null)
+        {
             Items.Remove(item);
+            gameEvent.InventoryChanged(this);
+        }
     }
 }
