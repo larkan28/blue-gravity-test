8b83d534d0ea84310d045b8dfc5f1e40150e1ffe
Facundo Lobo <faculobo200@gmail.com>
2023-09-13 09:40:47 -0300

    Added interact notification UI

---
+++ Assets/Scripts/UI/UI_Interaction.cs
@@ -0,0 +1,36 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class UI_Interaction : MonoBehaviour
+{
+    [SerializeField] private GameEvent gameEvent;
+    [SerializeField] private GameObject textContainer;
+    [SerializeField] private TextMeshProUGUI textInteraction;
+
+    private void OnEnable()
+    {
+        gameEvent.OnShowInteraction += OnShowInteraction;
+    }
+
+    private void OnDisable()
+    {
+        gameEvent.OnShowInteraction += OnShowInteraction;
+    }
+
+    private void Awake()
+    {
+        textContainer.SetActive(false);
+    }
+
+    private void OnShowInteraction(Interactable interaction)
+    {
+        if (interaction == null)
+            textContainer.SetActive(false);
+        else
+        {
+            textInteraction.text = interaction.GetText();
+            textContainer.SetActive(true);
+        }
+    }
+}
